f8be208136e21a9a22e2f9a3c9e653bed50e1c43
Omkar <omkarp050@gmail.com>
2024-10-10 13:24:19 +0530

    services done

--- toDo/src/app/task-service.service.ts
+++ toDo/src/app/task-service.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskServiceService {
-
-  constructor() { }
+  valid: any;
+  constructor(private http: HttpClient) {}
+  saveTask(data: any) {
+    console.log(data);
+    return this.http.post('http://localhost:8000/app/saveTask', data);
+  }
+  updateTask(data: any) {
+    return this.http.post('http://localhost:8000/app/updateTask', data);
+  }
+  getAllTasks() {
+    return this.http.get('http://localhost:8000/app/allList');
+  }
+  findTask(data: any) {
+    return this.http.post('http://localhost:8000/app/findTask', data);
+  }
+  deleteTask(data: any) {
+    return this.http.post('http://localhost:8000/app/deleteTask', data);
+  }
 }
